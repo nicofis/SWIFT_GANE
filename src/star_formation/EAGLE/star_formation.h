f1fddd46f70db716d459d7e3046055d8069b29b1
Nicolás <nicofische@gmail.com>
2026-09-18 11:55:47 +0200

    18-09-26 AM

--- src/star_formation/EAGLE/star_formation.h
+++ src/star_formation/EAGLE/star_formation.h
@@ -647,6 +647,7 @@ INLINE static void star_formation_copy_properties(
   /* Flag that this particle has not done feedback yet */
   sp->f_E = -1.f;
   sp->number_of_SNII_events = 0;
+  sp->number_of_HMXB_events = 0; // [GANE]
   sp->last_enrichment_time = sp->birth_time;
   sp->count_since_last_enrichment = -1;
   sp->number_of_heating_events = 0.;
